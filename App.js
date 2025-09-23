dda544f38082b862224d54f182ae08cc42c4bb6e
pedardok <Aclop28@gmail.com>
2025-09-23 22:11:34 +0800

    Edited App.js in Activity5

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import CounterApp from "./CounterApp";
 import ColorChangerApp from "./ColorChangerApp";
-import Messenger from "./Messenger";
 
 function App (){
   return (
     <>
-      <>Messenger</>
       <>CounterApp</>
       <>ColorChangerApp</>
     </>
